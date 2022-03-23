99d7b4a9c1e57e60d11742651ab9b0cb21d1deac
HW-21 <96047687+HW-21@users.noreply.github.com>
2022-03-23 09:15:30 +0800

    Update transfer.ts

--- fixtures/transfer.ts
+++ fixtures/transfer.ts
@@ -1,1 +1,33 @@
+import { ICurrenciesCode, ICurrencyCode } from '@currenxie/data/dist'
+import React, { FC, memo } from 'react'
+
+import { Box, FakeInput, Label } from '~components'
+import { useTranslation } from '~hooks'
+import { FormItemTemplate } from '~templates'
+
+interface IAccountBalanceWrapperProps {
+  balance: string
+  balanceCurrency: ICurrencyCode
+}
+
+export const AccountBalanceFieldWrapper: FC<IAccountBalanceWrapperProps> = memo(
+  ({ balanceCurrency, balance }) => {
+    const { t } = useTranslation('modals.transfer')
+    const isHKD = balanceCurrency === ICurrenciesCode.HKD
+    const labelText = t('labels.availableBalance')
+    const tooltip = isHKD ? t('.availableBalance.tooltip') : undefined
+
+    return (
+      <Box mb={3} flex={0}>
+        <FormItemTemplate
+          labelComponent={<Label text={labelText} fieldName="" />}
+          labelTooltip={tooltip}
+          labelTooltipTitle={labelText}
+        >
+          <FakeInput text={balance || ''} testId="availableBalance" />
+        </FormItemTemplate>
+      </Box>
+    )
+  },
+)
 
